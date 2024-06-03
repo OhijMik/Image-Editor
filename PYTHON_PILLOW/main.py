3c347515259e9e8e2ed75434011990c1321e5af4
OhijMik <jihok148@gmail.com>
2024-06-03 12:19:57 -0400

    Added cropping
    - Made the images to be cropped.

--- PYTHON_PILLOW/main.py
+++ PYTHON_PILLOW/main.py
@@ -1,5 +1,12 @@
 from PIL import Image
 
-beach1Img = Image.open("C:\\Users\\jihok\\GitHub\\Image-Editor\\PYTHON_PILLOW\\images\\beach1.jpg")
+images_path = "C:\\Users\\jihok\\GitHub\\Image-Editor\\PYTHON_PILLOW\\images\\"
+images_cropped_path = "C:\\Users\\jihok\\GitHub\\Image-Editor\\PYTHON_PILLOW\\images_cropped\\"
 
-beach1Img.show()
+beach1Img = Image.open(images_path + "beach1.jpg")
+
+# beach1Img.show()
+
+beach1_cropped = beach1Img.crop((50, 0, 150, 100))
+beach1_cropped.save(images_cropped_path + "beach1_cropped.jpg")
+beach1_cropped.show()
